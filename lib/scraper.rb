1861ea9c38b19e041b7fe3f90e10ac95f2602337
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-28 09:10:05 -0500

    commit tree 1e266107d3a14c27244ac670bf899d80430cab0a

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+print doc.css(".headline-26OIBN").text.strip
+courses = doc.css(".title-oE5vT4")
+
+courses.each do |course|
+    puts course.text.strip
+  end
